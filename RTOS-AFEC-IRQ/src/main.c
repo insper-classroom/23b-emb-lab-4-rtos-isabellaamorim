5e9fa10b1428c0ec9b5f830892c8c94a9f19658b
Isabella Amorim <isabellasa@al.insper.edu.br>
2023-09-20 11:00:27 -0300

    C Parte 2

--- RTOS-AFEC-IRQ/src/main.c
+++ RTOS-AFEC-IRQ/src/main.c
@@ -17,10 +17,13 @@
 /************************************************************************/
 
 #define TASK_ADC_STACK_SIZE (1024 * 10 / sizeof(portSTACK_TYPE))
-#define TASK_ADC_STACK_PRIORITY (tskIDLE_PRIORITY)
+#define TASK_ADC_STACK_PRIORITY (tskIDLE_PRIORITY+1)
+
+#define TASK_PROC_STACK_SIZE (1024 * 10 / sizeof(portSTACK_TYPE))
+#define TASK_PROC_STACK_PRIORITY (tskIDLE_PRIORITY)
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
-                                          signed char *pcTaskName);
+signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
 extern void vApplicationTickHook(void);
 extern void vApplicationMallocFailedHook(void);
@@ -34,9 +37,10 @@ TimerHandle_t xTimer;
 
 /** Queue for msg log send data */
 QueueHandle_t xQueueADC;
+QueueHandle_t xQueueSendData;
 
 typedef struct {
-  uint value;
+	uint value;
 } adcData;
 
 /************************************************************************/
@@ -45,7 +49,7 @@ typedef struct {
 
 static void USART1_init(void);
 static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
-                            afec_callback_t callback);
+afec_callback_t callback);
 static void configure_console(void);
 
 /************************************************************************/
@@ -53,13 +57,13 @@ static void configure_console(void);
 /************************************************************************/
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
-                                          signed char *pcTaskName) {
-  printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
-  /* If the parameters have been corrupted then inspect pxCurrentTCB to
-   * identify which task has overflowed its stack.
-   */
-  for (;;) {
-  }
+signed char *pcTaskName) {
+	printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
+	/* If the parameters have been corrupted then inspect pxCurrentTCB to
+	* identify which task has overflowed its stack.
+	*/
+	for (;;) {
+	}
 }
 
 extern void vApplicationIdleHook(void) { pmc_sleep(SAM_PM_SMODE_SLEEP_WFI); }
@@ -67,14 +71,14 @@ extern void vApplicationIdleHook(void) { pmc_sleep(SAM_PM_SMODE_SLEEP_WFI); }
 extern void vApplicationTickHook(void) {}
 
 extern void vApplicationMallocFailedHook(void) {
-  /* Called if a call to pvPortMalloc() fails because there is insufficient
-  free memory available in the FreeRTOS heap.  pvPortMalloc() is called
-  internally by FreeRTOS API functions that create tasks, queues, software
-  timers, and semaphores.  The size of the FreeRTOS heap is set by the
-  configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+	/* Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
 
-  /* Force an assert. */
-  configASSERT((volatile void *)NULL);
+	/* Force an assert. */
+	configASSERT((volatile void *)NULL);
 }
 
 /************************************************************************/
@@ -82,10 +86,10 @@ extern void vApplicationMallocFailedHook(void) {
 /************************************************************************/
 
 static void AFEC_pot_callback(void) {
-  adcData adc;
-  adc.value = afec_channel_get_value(AFEC_POT, AFEC_POT_CHANNEL);
-  BaseType_t xHigherPriorityTaskWoken = pdTRUE;
-  xQueueSendFromISR(xQueueADC, &adc, &xHigherPriorityTaskWoken);
+	adcData adc;
+	adc.value = afec_channel_get_value(AFEC_POT, AFEC_POT_CHANNEL);
+	BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+	xQueueSendFromISR(xQueueADC, &adc, &xHigherPriorityTaskWoken);
 }
 
 /************************************************************************/
@@ -93,43 +97,83 @@ static void AFEC_pot_callback(void) {
 /************************************************************************/
 
 void vTimerCallback(TimerHandle_t xTimer) {
-  /* Selecina canal e inicializa conversão */
-  afec_channel_enable(AFEC_POT, AFEC_POT_CHANNEL);
-  afec_start_software_conversion(AFEC_POT);
+	/* Selecina canal e inicializa conversão */
+	afec_channel_enable(AFEC_POT, AFEC_POT_CHANNEL);
+	afec_start_software_conversion(AFEC_POT);
+}
+
+static void task_proc(void *pvParameters) {
+	
+	config_AFEC_pot(AFEC_POT, AFEC_POT_ID, AFEC_POT_CHANNEL, AFEC_pot_callback);
+
+	xTimer = xTimerCreate(/* Just a text name, not used by the RTOS
+	kernel. */
+	"Timer",
+	/* The timer period in ticks, must be
+	greater than 0. */
+	100,
+	/* The timers will auto-reload themselves
+	when they expire. */
+	pdTRUE,
+	/* The ID is used to store a count of the
+	number of times the timer has expired, which
+	is initialised to 0. */
+	(void *)0,
+	/* Timer callback */
+	vTimerCallback);
+	xTimerStart(xTimer, 0);
+
+	// variável para recever dados da fila
+	adcData adc;
+	
+	while (1) {
+		if (xQueueReceive(xQueueADC, &(adc), 1000)) {
+			BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+			
+			int dados = 0;
+			for(int i=0; i<11; i++){
+				dados += adc.value;
+			}
+			int media = dados/10;
+			
+			xQueueSend(xQueueSendData, &media, &xHigherPriorityTaskWoken);
+		}
+	}
 }
 
 static void task_adc(void *pvParameters) {
 
-  // configura ADC e TC para controlar a leitura
-  config_AFEC_pot(AFEC_POT, AFEC_POT_ID, AFEC_POT_CHANNEL, AFEC_pot_callback);
+	config_AFEC_pot(AFEC_POT, AFEC_POT_ID, AFEC_POT_CHANNEL, AFEC_pot_callback);
 
-  xTimer = xTimerCreate(/* Just a text name, not used by the RTOS
-                        kernel. */
-                        "Timer",
-                        /* The timer period in ticks, must be
-                        greater than 0. */
-                        100,
-                        /* The timers will auto-reload themselves
-                        when they expire. */
-                        pdTRUE,
-                        /* The ID is used to store a count of the
-                        number of times the timer has expired, which
-                        is initialised to 0. */
-                        (void *)0,
-                        /* Timer callback */
-                        vTimerCallback);
-  xTimerStart(xTimer, 0);
+	xTimer = xTimerCreate(/* Just a text name, not used by the RTOS
+	kernel. */
+	"Timer",
+	/* The timer period in ticks, must be
+	greater than 0. */
+	100,
+	/* The timers will auto-reload themselves
+	when they expire. */
+	pdTRUE,
+	/* The ID is used to store a count of the
+	number of times the timer has expired, which
+	is initialised to 0. */
+	(void *)0,
+	/* Timer callback */
+	vTimerCallback);
+	xTimerStart(xTimer, 0);
 
-  // variável para recever dados da fila
-  adcData adc;
+	// variável para recever dados da fila
+	adcData adc;
 
-  while (1) {
-    if (xQueueReceive(xQueueADC, &(adc), 1000)) {
-      printf("ADC: %d \n", adc.value);
-    } else {
-      printf("Nao chegou um novo dado em 1 segundo");
-    }
-  }
+	int media;
+	
+	while (1) {
+		if (xQueueReceive(xQueueSendData, &media, 1000)) {
+			printf("ADC MEDIA: %d \n", media); // Removed the ampersand before media.
+		} else {
+			printf("Nao chegou um novo dado em 1 segundo");
+		}
+	}
 }
 
 /************************************************************************/
@@ -137,66 +181,66 @@ static void task_adc(void *pvParameters) {
 /************************************************************************/
 
 /**
- * \brief Configure the console UART.
- */
+* \brief Configure the console UART.
+*/
 static void configure_console(void) {
-  const usart_serial_options_t uart_serial_options = {
-      .baudrate = CONF_UART_BAUDRATE,
-      .charlength = CONF_UART_CHAR_LENGTH,
-      .paritytype = CONF_UART_PARITY,
-      .stopbits = CONF_UART_STOP_BITS,
-  };
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = CONF_UART_BAUDRATE,
+		.charlength = CONF_UART_CHAR_LENGTH,
+		.paritytype = CONF_UART_PARITY,
+		.stopbits = CONF_UART_STOP_BITS,
+	};
 
-  /* Configure console UART. */
-  stdio_serial_init(CONF_UART, &uart_serial_options);
+	/* Configure console UART. */
+	stdio_serial_init(CONF_UART, &uart_serial_options);
 
-  /* Specify that stdout should not be buffered. */
-  setbuf(stdout, NULL);
+	/* Specify that stdout should not be buffered. */
+	setbuf(stdout, NULL);
 }
 
 static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
-                            afec_callback_t callback) {
-  /*************************************
-   * Ativa e configura AFEC
-   *************************************/
-  /* Ativa AFEC - 0 */
-  afec_enable(afec);
+afec_callback_t callback) {
+	/*************************************
+	* Ativa e configura AFEC
+	*************************************/
+	/* Ativa AFEC - 0 */
+	afec_enable(afec);
 
-  /* struct de configuracao do AFEC */
-  struct afec_config afec_cfg;
+	/* struct de configuracao do AFEC */
+	struct afec_config afec_cfg;
 
-  /* Carrega parametros padrao */
-  afec_get_config_defaults(&afec_cfg);
+	/* Carrega parametros padrao */
+	afec_get_config_defaults(&afec_cfg);
 
-  /* Configura AFEC */
-  afec_init(afec, &afec_cfg);
+	/* Configura AFEC */
+	afec_init(afec, &afec_cfg);
 
-  /* Configura trigger por software */
-  afec_set_trigger(afec, AFEC_TRIG_SW);
+	/* Configura trigger por software */
+	afec_set_trigger(afec, AFEC_TRIG_SW);
 
-  /*** Configuracao específica do canal AFEC ***/
-  struct afec_ch_config afec_ch_cfg;
-  afec_ch_get_config_defaults(&afec_ch_cfg);
-  afec_ch_cfg.gain = AFEC_GAINVALUE_0;
-  afec_ch_set_config(afec, afec_channel, &afec_ch_cfg);
+	/*** Configuracao específica do canal AFEC ***/
+	struct afec_ch_config afec_ch_cfg;
+	afec_ch_get_config_defaults(&afec_ch_cfg);
+	afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+	afec_ch_set_config(afec, afec_channel, &afec_ch_cfg);
 
-  /*
-  * Calibracao:
-  * Because the internal ADC offset is 0x200, it should cancel it and shift
-  down to 0.
-  */
-  afec_channel_set_analog_offset(afec, afec_channel, 0x200);
+	/*
+	* Calibracao:
+	* Because the internal ADC offset is 0x200, it should cancel it and shift
+	down to 0.
+	*/
+	afec_channel_set_analog_offset(afec, afec_channel, 0x200);
 
-  /***  Configura sensor de temperatura ***/
-  struct afec_temp_sensor_config afec_temp_sensor_cfg;
+	/***  Configura sensor de temperatura ***/
+	struct afec_temp_sensor_config afec_temp_sensor_cfg;
 
-  afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
-  afec_temp_sensor_set_config(afec, &afec_temp_sensor_cfg);
+	afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
+	afec_temp_sensor_set_config(afec, &afec_temp_sensor_cfg);
 
-  /* configura IRQ */
-  afec_set_callback(afec, afec_channel, callback, 1);
-  NVIC_SetPriority(afec_id, 4);
-  NVIC_EnableIRQ(afec_id);
+	/* configura IRQ */
+	afec_set_callback(afec, afec_channel, callback, 1);
+	NVIC_SetPriority(afec_id, 4);
+	NVIC_EnableIRQ(afec_id);
 }
 
 /************************************************************************/
@@ -204,30 +248,39 @@ static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
 /************************************************************************/
 
 /**
- *  \brief FreeRTOS Real Time Kernel example entry point.
- *
- *  \return Unused (ANSI-C compatibility).
- */
+*  \brief FreeRTOS Real Time Kernel example entry point.
+*
+*  \return Unused (ANSI-C compatibility).
+*/
 int main(void) {
-  sysclk_init();
-  board_init();
-  configure_console();
+	sysclk_init();
+	board_init();
+	configure_console();
 
-  xQueueADC = xQueueCreate(100, sizeof(adcData));
-  if (xQueueADC == NULL)
-    printf("falha em criar a queue xQueueADC \n");
+	xQueueADC = xQueueCreate(100, sizeof(adcData));
+	if (xQueueADC == NULL)
+	printf("falha em criar a queue xQueueADC \n");
+	
+	xQueueSendData = xQueueCreate(100, sizeof(adcData));
+	if (xQueueSendData == NULL)
+	printf("falha em criar a queue xQueueSendData \n");
 
-  if (xTaskCreate(task_adc, "ADC", TASK_ADC_STACK_SIZE, NULL,
-                  TASK_ADC_STACK_PRIORITY, NULL) != pdPASS) {
-    printf("Failed to create test ADC task\r\n");
-  }
+	if (xTaskCreate(task_adc, "ADC", TASK_ADC_STACK_SIZE, NULL,
+	TASK_ADC_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test ADC task\r\n");
+	}
+	
+	if (xTaskCreate(task_proc, "PROC", TASK_PROC_STACK_SIZE, NULL,
+	TASK_PROC_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test proc task\r\n");
+	}
 
-  vTaskStartScheduler();
+	vTaskStartScheduler();
 
-  while (1) {
-  }
+	while (1) {
+	}
 
-  /* Will only get here if there was insufficient memory to create the idle
-   * task. */
-  return 0;
+	/* Will only get here if there was insufficient memory to create the idle
+	* task. */
+	return 0;
 }
